2e164c973f82242599cef433068936aaf3b4649e
PAD <ala.garbaa.pro@gmail.com>
2024-01-28 20:15:14 +0100

    Update +page.svelte

--- src/routes/(content-management)/web-content-coordinator/dashboard/+page.svelte
+++ src/routes/(content-management)/web-content-coordinator/dashboard/+page.svelte
@@ -12,6 +12,7 @@
 			<div class="px-4 sm:px-0">
 				<h2 class="text-base font-semibold leading-7 text-gray-900">Website Info</h2>
 				<p class="mt-1 text-sm leading-6 text-gray-600">
+					The following parking information will be publicly displayed
 				</p>
 			</div>
 
