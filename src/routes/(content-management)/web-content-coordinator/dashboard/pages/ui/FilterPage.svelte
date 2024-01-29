6f30a75bd0c8dbf26384daffe0499e185eff173a
PAD <ala.garbaa.pro@gmail.com>
2024-01-29 20:24:50 +0100

    Update FilterPage.svelte

--- src/routes/(content-management)/web-content-coordinator/dashboard/pages/ui/FilterPage.svelte
+++ src/routes/(content-management)/web-content-coordinator/dashboard/pages/ui/FilterPage.svelte
@@ -210,19 +210,6 @@
 	</p>
 
 	{#if $isPageStoreLoading}
-		<div class="w-full max-w-sm p-4 mt-4 border border-blue-300 rounded-md shadow">
-			<div class="flex space-x-4 animate-pulse">
-				<div class="flex-1 py-1 space-y-6">
-					<div class="h-2 rounded bg-slate-700"></div>
-					<div class="space-y-3">
-						<div class="grid grid-cols-3 gap-4">
-							<div class="h-2 col-span-2 rounded bg-slate-700"></div>
-							<div class="h-2 col-span-1 rounded bg-slate-700"></div>
-						</div>
-						<div class="h-2 rounded bg-slate-700"></div>
-					</div>
-				</div>
-			</div>
-		</div>
+		
 	{/if}
 {/if}
