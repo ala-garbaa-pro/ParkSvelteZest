cee31ed6581eed9d1365b5d248b0e98fe0e7debb
PAD <ala.garbaa.pro@gmail.com>
2024-01-30 19:59:27 +0100

    Update getTextsByContent.ts

--- src/routes/(content-management)/web-content-coordinator/dashboard/pages/functions/getTextsByContent.ts
+++ src/routes/(content-management)/web-content-coordinator/dashboard/pages/functions/getTextsByContent.ts
@@ -6,5 +6,23 @@ type Props = {
 
 export const getTextsByContent = async ({ contentID }: Props) => {
     try {
-    
+        const response = await fetch(`${wccPagesPath}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ action: "getTextsByContent", contentID }),
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+
+            return data
+        } else {
+            // Handle errors here
+            console.error('Error:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
 };
