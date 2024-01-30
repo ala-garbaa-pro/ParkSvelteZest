25da32175a002b79949cc38ea82e9cb53cd7f663
PAD <ala.garbaa.pro@gmail.com>
2024-01-30 20:00:01 +0100

    Update getTextsByContent.ts

--- src/routes/(content-management)/web-content-coordinator/dashboard/pages/functions/getTextsByContent.ts
+++ src/routes/(content-management)/web-content-coordinator/dashboard/pages/functions/getTextsByContent.ts
@@ -14,7 +14,14 @@ export const getTextsByContent = async ({ contentID }: Props) => {
             body: JSON.stringify({ action: "getTextsByContent", contentID }),
         });
 
-    
+        if (response.ok) {
+            const data = await response.json();
+
+            return data
+        } else {
+            // Handle errors here
+            console.error('Error:', response.statusText);
+        }
     } catch (error) {
         console.error('Error fetching data:', error);
     }
