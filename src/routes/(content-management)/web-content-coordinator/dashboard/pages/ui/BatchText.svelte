06dd8adbd9b079db02a9f24fb1e54e4d11cfe389
PAD <ala.garbaa.pro@gmail.com>
2024-01-31 20:24:23 +0100

    Update BatchText.svelte

--- src/routes/(content-management)/web-content-coordinator/dashboard/pages/ui/BatchText.svelte
+++ src/routes/(content-management)/web-content-coordinator/dashboard/pages/ui/BatchText.svelte
@@ -2,6 +2,7 @@
 	import { selectedContent } from '$lib/stores/pageStore';
 </script>
 <hr />
+<h1>{$selectedContent.key}</h1>
 
 <textarea name="inputData" id="inputData" cols="30" rows="20"></textarea>
 
